04bddca77c281d40cbb69bd1c5272fc3abd71b5e
Steven Carranza <steven.carranza@priceline.com>
2024-09-09 14:32:31 -0500

    testing workflow

---
+++ main.py
@@ -0,0 +1,10 @@
+import pandas
+import os
+
+
+if __name__ == '__main__':
+    if os.getenv('TEST_VAR') == 'mysecret':
+        print("matched secret")
+
+    print(os.getenv('Test_VAR', "????"))
+    print(pandas.__version__)
